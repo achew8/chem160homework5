d188d954f47627083338513a3384367d12d133fc
achew8 <70230739+achew8@users.noreply.github.com>
2020-10-04 21:33:17 -0700

    Update hyperc.py

--- hyperc.py
+++ hyperc.py
@@ -5,13 +5,12 @@ dist=0
 start_time=time.process_time()
 for i in range(ntrials):
     x1=random.random()
-    x2=random.random()
     y1=random.random()
-    y2=random.random()
     z1=random.random()
+    x2=random.random()
+    y2=random.random()
     z2=random.random()
     dist+=math.sqrt((x1-x2)**2+(y1-y2)**2+(z1-z2)**2)
-
 dist/=ntrials
 e_time=time.process_time()-start_time
 ex_dist=1/105*(4+17*math.sqrt(2)-6*math.sqrt(3)+21*math.log(1+math.sqrt(2))+42*math.log(2+math.sqrt(3))-7*math.pi)

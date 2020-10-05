cae230d2688cddd590953052c44a0583952ffad2
achew8 <70230739+achew8@users.noreply.github.com>
2020-10-04 17:56:04 -0700

    Update molemass.py

--- molemass.py
+++ molemass.py
@@ -1,6 +1,6 @@
 def molemass(sequence):
     symbols = ["H", "C", "N", "O", "P", "S", "K", "F"]
-    masses = [1.00784, 12.0107, 14.0067, 15.999, 30.973762, 32.065 , 39.0983, 18.998403]
+    masses = [1.0079, 12.0107, 14.0067, 15.9994, 30.9738, 32.0660 , 39.0983, 18.9984]
     atoms_dict = dict(zip(symbols, masses))
     total_mass = 0
     for i in range(len(sequence)):
